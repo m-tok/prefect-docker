a787a09d5886cb7f9a5ecd4ddebe040943afa73f
m-tok <meneksetok1907@gmail.com>
2023-07-27 11:59:06 -0700

    cat_flow added

--- cat_flow.py
+++ cat_flow.py
@@ -2,12 +2,12 @@ from prefect import flow, task, get_run_logger
 
 @task
 def speak():
-    get_run_logger().info("Woof!")
+    get_run_logger().info("Meoow!")
 
 @flow
-def dog(num_barks: int):
-    for i in range(num_barks):
+def cat(num_meows: int):
+    for i in range(num_meows):
         speak()
 
 if __name__ == "__main__":
-    dog(3)
+    cat(3)
